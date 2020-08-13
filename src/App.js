5088b7f1a508412c51ca4016859e16811261cb55
muNeves3 <murilogrilosn@gmail.com>
2020-08-13 19:05:05 -0300

    :rocket: main code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import Button from "./components/Button";
+import Input from "./components/Input";
+import ClearButton from "./components/ClearButton";
 
 function App() {
+  const [input, setInput] = useState("");
+  var [previousNumber, setPreviousNumber] = useState("");
+  var [currentNumber, setCurrentNumber] = useState("");
+  const [operator, setOperator] = useState("");
+
+  const handleClick = (val) => {
+    setInput(input + val);
+  };
+
+  const addDecimal = (val) => {
+    //only add decimal if there is no current decimal point present in the input area
+    if (input.indexOf(".") === -1) {
+      setInput(input + val);
+    }
+  };
+
+  const addZeroToInput = (val) => {
+    //if this.state.input is not empty then add zero
+    if (input !== "") {
+      setInput(input + val);
+    }
+  };
+
+  const clearInput = () => {
+    setInput("");
+  };
+
+  const add = () => {
+    setPreviousNumber((previousNumber = input));
+    setInput("");
+    setOperator("plus");
+  };
+
+  const subtract = () => {
+    setPreviousNumber((previousNumber = input));
+    setInput("");
+    setOperator("minus");
+  };
+
+  const multiply = () => {
+    setPreviousNumber((previousNumber = input));
+    setInput("");
+    setOperator("times");
+  };
+
+  const divide = () => {
+    setPreviousNumber((previousNumber = input));
+    setInput("");
+    setOperator("divide");
+  };
+
+  const evaluate = () => {
+    setCurrentNumber((currentNumber = input));
+
+    if (operator === "plus") {
+      setInput(parseFloat(previousNumber) + parseFloat(currentNumber));
+    }
+
+    if (operator === "minus") {
+      setInput(parseFloat(previousNumber) - parseFloat(currentNumber));
+    }
+
+    if (operator === "times") {
+      setInput(parseFloat(previousNumber) * parseFloat(currentNumber));
+    }
+
+    if (operator === "divide") {
+      setInput(parseFloat(previousNumber) / parseFloat(currentNumber));
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calc-wrapper">
+        <div className="row">
+          <Input>{input}</Input>
+        </div>
+
+        <div className="row">
+          <Button handleClick={handleClick} number="7" />
+          <Button handleClick={handleClick} number="8" />
+          <Button handleClick={handleClick} number="9" />
+          <Button handleClick={divide} number="/" />
+        </div>
+
+        <div className="row">
+          <Button handleClick={handleClick} number="4" />
+          <Button handleClick={handleClick} number="5" />
+          <Button handleClick={handleClick} number="6" />
+          <Button handleClick={multiply} number="*" />
+        </div>
+
+        <div className="row">
+          <Button handleClick={handleClick} number="1" />
+          <Button handleClick={handleClick} number="2" />
+          <Button handleClick={handleClick} number="3" />
+          <Button handleClick={add} number="+" />
+        </div>
+
+        <div className="row">
+          <Button handleClick={addDecimal} number="." />
+          <Button handleClick={addZeroToInput} number="0" />
+          <Button handleClick={evaluate} number="=" />
+          <Button handleClick={subtract} number="-" />
+        </div>
+
+        <div className="row">
+          <ClearButton handleClear={clearInput} children="Clear" />
+        </div>
+      </div>
     </div>
   );
 }
